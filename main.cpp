954042e9637ae78f744bd49796bd5aa06fb468e6
Fatih Kemal Terzi <fatihkemalterzi@gmail.com>
2024-03-17 04:45:36 +0300

    camera test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
 using namespace std;
+using namespace cv;
+
+/*void main() {
+	
+	string path = "images.jpeg";
+	Mat img = imread(path);
+	imshow("Frame", img);
+	waitKey(0);
+	
+}*/
 
 int main() {
-
-	cout << "First commit check" << endl;
+	VideoCapture video(0);
+	Mat img;
+	while (true) {
+		video.read(img);
+		imshow("Frame", img);
+		waitKey(1);
+	}
 	return 0;
 }
